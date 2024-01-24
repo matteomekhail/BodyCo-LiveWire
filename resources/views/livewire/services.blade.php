15a0279e1be514438d2ef66ce1baac54fa876550
matteomekhail <matteomekhail04@gmail.com>
2024-01-25 08:27:16 +1100

    Refactor service images layout

--- resources/views/livewire/services.blade.php
+++ resources/views/livewire/services.blade.php
@@ -3,24 +3,25 @@
         Our Services
     </h2>
 
-    <div class="mt-12 grid grid-cols-2 justify-center gap-8 sm:grid-cols-3 md:grid-cols-4">
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+    <div class="mt-12 px-4 sm:px-0 grid grid-cols-2 justify-center gap-8 sm:grid-cols-3 md:grid-cols-4">
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="/images/SpayTan.jpg" alt="google logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/lush.jpg" alt="microsoft logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/brown2.jpg" alt="netflix logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/sauna.jpg" alt="spotify logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/cryo.jpg" alt="paypal logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/sculpt.jpg" alt="paypal logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="/images/logo/paypal.svg" alt="paypal logo" />
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-full transition-all duration-500 sm:inline"
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="/images/logo/paypal.svg" alt="paypal logo" />
     </div>
+
     <br>
     <br>
     <br>
