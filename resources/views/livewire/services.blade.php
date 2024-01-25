d15d088821e456e64ecb1819696983ce7cf39353
matteomekhail <matteomekhail04@gmail.com>
2024-01-25 11:07:45 +1100

    Delete unused images and update image sources

--- resources/views/livewire/services.blade.php
+++ resources/views/livewire/services.blade.php
@@ -4,12 +4,14 @@
     </h2>
 
     <div class="mt-12 px-4 sm:px-0 grid grid-cols-2 justify-center gap-8 sm:grid-cols-3 md:grid-cols-4">
-        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
-            src="/images/SpayTan.jpg" alt="google logo" />
+
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/lush.jpg" alt="microsoft logo" />
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/brown2.jpg" alt="netflix logo" />
+
+        <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
+            src="/images/SpayTan.jpg" alt="google logo" />
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/sauna.jpg" alt="spotify logo" />
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
@@ -17,9 +19,9 @@
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
             src="images/sculpt.jpg" alt="paypal logo" />
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
-            src="/images/logo/paypal.svg" alt="paypal logo" />
+            src="images/boh.jpg" alt="paypal logo" />
         <img class="mx-auto h-56 w-56 cursor-pointer object-cover rounded-t-full transition-all duration-500 sm:inline"
-            src="/images/logo/paypal.svg" alt="paypal logo" />
+            src="/images/uomo.jpg" alt="paypal logo" />
     </div>
 
     <br>
